b5edb6847e0580d79d1223a54540255fce40519b
krateng <git.noreply@krateng.ch>
2023-08-25 04:52:52 +0200

    Database backup

--- signalGPT/__init__.py
+++ signalGPT/__init__.py
@@ -1,9 +1,15 @@
 import yaml
 import openai
+from datetime import datetime
+import shutil
+import os
 
 with open('config.yml','r') as fd:
 	config = yaml.load(fd,Loader=yaml.SafeLoader)
-	
+
 
 openai.api_key = config['apikey']
 
+
+backupfile = os.path.join("backups",datetime.utcnow().strftime("%Y-%m-%d") + ".sqlite")
+shutil.copyfile("database.sqlite",backupfile)
