4a9134a3c476c19e0e21bc3bb735c46cc7f6b79c
krateng <git.noreply@krateng.ch>
2023-08-26 16:59:34 +0200

    Setup QoL

--- signalGPT/__init__.py
+++ signalGPT/__init__.py
@@ -4,8 +4,25 @@ from datetime import datetime
 import shutil
 import os
 
-with open('config.yml','r') as fd:
-	config = yaml.load(fd,Loader=yaml.SafeLoader)
+try:
+	with open('config.yml','r') as fd:
+		config = yaml.load(fd,Loader=yaml.SafeLoader)
+except FileNotFoundError:
+	with open('config.yml','w') as fd:
+		config = {
+			'apikey':'0',
+			'model':'gpt-3.5-turbo-16k-0613',
+			'user':{
+				'name':'Anon',
+				'handle':'anon',
+				'description':'I am mysterious and unknowable. Love waffles.',
+				'preferred_emojis':'😃🪷🇰🇷'
+			}
+		}
+		yaml.dump(config,fd)
+
+for folder in ['media','contacts','conversations','backups']:
+	os.makedirs(folder,exist_ok=True)
 
 
 openai.api_key = config['apikey']
